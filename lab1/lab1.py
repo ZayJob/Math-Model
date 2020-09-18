347e18fd46ced7775744b9c992ab146351c58a83
Vlad Bubeniuk <ZayByst@mail.ru>
2020-09-18 04:26:08 +0300

    Wrote 2 methods

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,53 +1,14 @@
 """LAB 1."""
 
-
+from random import randint, seed
 from abc import ABC, abstractmethod
+
+from matplotlib import pyplot as plt
 
 
 class NotInheritedFromBaseClass(Exception):
     def __init__(self, text):
         self.txt = text
-
-
-class BaseTest(ABC):
-    """Absatract base class for test class"""
-    @abstractmethod
-    def build_plot(self) -> None:
-        """"""
-        pass
-
-    @abstractmethod
-    def __str__(self) -> str:
-        """"""
-        return self.__class__.__name__
-
-
-class UniformityTest(BaseTest):
-    """Uniformity test class"""
-    def __init__(self):
-        pass
-
-    def build_plot(self) -> None:
-        """"""
-        print("UniformityTest")
-
-    def __str__(self) -> str:
-        """"""
-        super().__str__()
-
-
-class IndependenceTest(BaseTest):
-    """Independence test class"""
-    def __init__(self):
-        pass
-
-    def build_plot(self) -> None:
-        """"""
-        print("IndependenceTest")
-
-    def __str__(self) -> str:
-        """"""
-        super().__str__()
 
 
 class BaseMethod(ABC):
@@ -70,16 +31,35 @@ class BaseMethod(ABC):
 
 class MidSquareMethod(BaseMethod):
     """Mid-square method class"""
-    def __init__(self):
-        pass
+    def __init__(self, n: int):
+        self.n: int = n
+        self.result: list = []
 
     def solve(self) -> None:
         """"""
-        pass
+        rand_int = randint(1000,9999)
 
-    def start(self) -> None:
+        i = 0
+        while i < self.n:
+            rand_int = rand_int ** 2
+            if len(str(rand_int)) < 8:
+                rand_int = "0" * (8 - len(str(rand_int))) + str(rand_int)
+            else:
+                rand_int = str(rand_int)
+
+            self.result.append(int(rand_int[1:5]))
+            rand_int = int(rand_int[1:5])
+
+            i += 1
+
+
+    def start(self) -> list:
         """"""
-        print("MidSquareMethod")
+        seed(randint(1,100))
+
+        self.solve()
+
+        return self.result
 
     def __str__(self) -> str:
         """"""
@@ -88,16 +68,73 @@ class MidSquareMethod(BaseMethod):
 
 class MultiplicativeCongruentMethod(BaseMethod):
     """Multiplicative congruent method class"""
-    def __init__(self):
-        pass
+    def __init__(self, n: int, k: int, m: int):
+        self.n: int = n
+        self.k: int = k
+        self.m: int = m
+        self.result: list = []
 
     def solve(self) -> None:
         """"""
-        pass
+        rand_int = randint(1000,9999)
+
+        i = 0
+        while i < self.n:
+            rand_int = (self.k * rand_int) % self.m
+
+            self.result.append(rand_int / self.m)
+
+            i += 1
+
 
     def start(self) -> None:
         """"""
-        print("MultiplicativeCongruentMethod")
+        seed(randint(100,200))
+
+        self.solve()
+
+        return self.result
+
+    def __str__(self) -> str:
+        """"""
+        super().__str__()
+
+
+class BaseTest(ABC):
+    """Absatract base class for test class"""
+    @abstractmethod
+    def build_plot(self) -> None:
+        """"""
+        pass
+
+    @abstractmethod
+    def __str__(self) -> str:
+        """"""
+        return self.__class__.__name__
+
+
+class UniformityTest(BaseTest):
+    """Uniformity test class"""
+    def __init__(self):
+        pass
+
+    def build_plot(self, result: list) -> None:
+        """"""
+        pass
+
+    def __str__(self) -> str:
+        """"""
+        super().__str__()
+
+
+class IndependenceTest(BaseTest):
+    """Independence test class"""
+    def __init__(self):
+        pass
+
+    def build_plot(self, result: list) -> None:
+        """"""
+        print("IndependenceTest")
 
     def __str__(self) -> str:
         """"""
@@ -111,14 +148,14 @@ class LAB:
 
     def add_tests(self, *test_objects: tuple) -> None:
         """"""
-        self._tests: list = list(test_objects)
+        self.tests: list = list(test_objects)
 
     def _checking_inheritance(self, list_object: list, base_class) -> None:
         """"""
         try:
             for index, obj in enumerate(list_object):
                 if not isinstance(obj, base_class):
-                    error_text = "{0} not inherited from {1} abstract class".format(obj, base_class)
+                    error_text = "{0} not inherited from {1} abstract class".format(obj, base_class.__name__)
                     del list_object[index]
                     raise NotInheritedFromBaseClass(error_text)
                 if base_class is BaseMethod:
@@ -155,20 +192,39 @@ class LAB:
     def run(self) -> None:
         """Function for run all methods"""
         for method in self.methods:
-            method.start()
-        for test in self.tests:
-            test.build_plot()
+            result = method.start()
+            for test in self.tests:
+                test.build_plot(result)
+
+# TEST
+class TestRaise():
+    """TestRaise class"""
+    def __init__(self):
+        pass
+
+    def solve(self) -> None:
+        """"""
+        pass
+
+    def start(self) -> None:
+        """"""
+        print("MultiplicativeCongruentMethod")
+
+    def __str__(self) -> str:
+        """"""
+        return self.__class__.__name__
 
 
 def main() -> None:
     """Main"""
-    midsquare_method = MidSquareMethod()
-    multiplicative_congruent_method = MultiplicativeCongruentMethod()
+    midsquare_method = MidSquareMethod(5)
+    multiplicative_congruent_method = MultiplicativeCongruentMethod(20, 7, 13)
+    test_raise = TestRaise()
 
     independence_test = IndependenceTest()
     uniformity_test= UniformityTest()
 
-    lab = LAB(midsquare_method, multiplicative_congruent_method)
+    lab = LAB(midsquare_method, multiplicative_congruent_method, test_raise)
     lab.add_tests(independence_test, uniformity_test)
     lab.run()
 
