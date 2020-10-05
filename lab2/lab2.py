ef0c6ed11cf10a0c7d6fd6d11fa619b390e7e0be
Vlad Bubeniuk <ZayByst@mail.ru>
2020-10-05 23:27:57 +0300

    lab2 part 1

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -4,16 +4,17 @@ import sys
 import math
 
 import numpy as np
-import scipy as sp
 
+import scipy.integrate as integrate
 from matplotlib import pyplot as plt
-from random import randint, seed
+
+from random import random
 from abc import ABC, abstractmethod
 
 
 def func(x: float, y: float) -> float:
     """Func"""
-    return math.exp(-x * -y)
+    return math.exp(-x-y)
 
 
 class NotInheritedFromBaseClass(Exception):
@@ -44,17 +45,56 @@ class CheckingInheritance:
             sys.exit()
 
 
-class BaseMethod(ABC):
-    """Absatract base class for method class"""
+class BaseStatisticalResearch(ABC):
+    """."""
     @abstractmethod
-    def solve(self) -> None:
-        """Solve method."""
+    def M(self, x: list) -> float:
+        """Сheckmate waiting M(x)."""
+        return 1/len(x) * sum(x)
+
+    @abstractmethod
+    def D(self, x: list) -> float:
+        """Dispersion D(x)."""
+        return 1/len(x) * sum(np.array(x) ** 2 - self.M(x) ** 2)
+
+    @abstractmethod
+    def M_x_y(self, x: list, y: list) -> float:
+        """Сheckmate waiting M(xy)"""
+        return sum([i * j for i, j in zip(x, y)]) * 1/len(x)
+
+    @abstractmethod
+    def corrcoef(self, x: list, y: list) -> None:
+        """Calculate and print corr coef."""
+        return (self.M_x_y(x, y) - self.M(x) * self.M(y)) / math.sqrt(self.D(x) * self.D(y))
+
+    @abstractmethod
+    def theoretical_M(self, a: int, b: int, by='x') -> float:
+        """Сheckmate waiting M(x)."""
         pass
 
     @abstractmethod
-    def start(self) -> list:
-        """Start method."""
+    def theoretical_D(self, a: int, b: int, by='x') -> float:
+        """Dispersion D(x)."""
         pass
+
+    @abstractmethod
+    def theoretical_M_x_y(self) -> float:
+        """Сheckmate waiting M(xy)"""
+        pass
+
+    @abstractmethod
+    def theoretical_corrcoef(self, a:int, b: int) -> float:
+        """Calculate and print corr coef."""
+        pass
+
+    @abstractmethod
+    def function_interval(self, func: object, n: int, list_values: list) -> tuple:
+        """."""
+        result_list = [func(np.random.choice(list_values, n, replace=True)) for _ in range(n)]
+        var = np.var(result_list)
+        sigma = np.sqrt(var)
+        result = func(list_values)
+        return (result - 3 * sigma, result + 3 * sigma)
 
     @abstractmethod
     def __str__(self) -> str:
@@ -62,33 +102,140 @@ class BaseMethod(ABC):
         return self.__class__.__name__
 
 
-class MultiplicativeCongruentMethod(BaseMethod):
-    """Multiplicative congruent method class"""
-    def __init__(self, n: int, k: int, m: int):
-        self.n: int = n
-        self.k: int = k
-        self.m: int = m
-        self.result: list = []
+class MixinHist:
+    """."""
+    def __init__(self):
+        pass
 
-    def solve(self) -> None:
-        """Solve multiplicative congruent method."""
-        rand_int = randint(1000,9999)
+    def component_vectors_histogram(self, x: list, y: list) -> None:
+        """."""
+        x, y = np.meshgrid(sorted(x), sorted(y))
+        z = np.exp(-x-y)
+        fig = plt.figure()
+        ax = plt.axes(projection='3d')
+        ax.set_ylabel("y")
+        ax.set_xlabel("x")
+        ax.set_title("3D hist")
+        ax.plot_wireframe(x, y, z)
+        plt.show()
 
-        i = 0
-        while i < self.n:
-            rand_int = (self.k * rand_int) % self.m
 
-            self.result.append(rand_int / self.m)
+class MixinEmpiricalFunction:
+    """."""
+    def __init__(self):
+        pass
 
-            i += 1
+    def empirical_function(self, x: list, y: list) -> None:
+        """."""
+        pass
 
-    def start(self) -> list:
-        """Start solve method."""
-        seed(randint(100,200))
 
-        self.solve()
+class CRVStatisticalResearch(BaseStatisticalResearch, MixinHist):
+    """."""
+    def __init__(self) -> None:
+        pass
 
-        return self.result
+    def M(self, x: list) -> float:
+        """Сheckmate waiting M(x)."""
+        return super().M(x)
+
+    def D(self, x: list) -> float:
+        """Dispersion D(x)."""
+        return super().D(x)
+
+    def M_x_y(self, x: list, y: list) -> float:
+        """Сheckmate waiting M(xy)"""
+        return super().M_x_y(x, y)
+
+    def corrcoef(self, x: list, y: list) -> None:
+        """Calculate and print corr coef."""
+        return super().corrcoef(x, y)
+
+    def theoretical_M(self, a: int, b: int, by='x') -> float:
+        """Сheckmate waiting theoretical_M(x)."""
+        l_f = lambda x, y: func(x, y) * (eval(by))
+        l_a = lambda x: a
+        l_b = lambda x: b
+        return integrate.dblquad(l_f, a, b, l_a, l_b)[0]
+
+    def theoretical_D(self, a: int, b: int, by='x') -> float:
+        """Dispersion theoretical_D(x)."""
+        l_f = lambda x, y: func(x, y) * (eval(by)) ** 2
+        l_a = lambda x: a
+        l_b = lambda x: b
+        return integrate.dblquad(l_f, a, b, l_a, l_b)[0] - self.theoretical_M(a, b, by) ** 2
+
+    def theoretical_M_x_y(self, a: int, b: int) -> float:
+        """Сheckmate waiting theoretical_M_x_y(xy)"""
+        return self.theoretical_M(a, b, by='x * y')
+
+    def theoretical_corrcoef(self, a:int, b: int) -> float:
+        """Calculate and print corr coef."""
+        return (self.theoretical_M_x_y(a, b) - self.theoretical_M(a, b, by='x') * self.theoretical_M(a, b, by='y')) / math.sqrt(self.theoretical_D(a, b, by='x') * self.theoretical_D(a, b, by='y'))
+
+    def function_interval(self, func: object, n: int, list_values: list) -> tuple:
+        """."""
+        return super().function_interval(func, n, list_values)
+
+    def component_vectors_histogram(self, x: list, y: list) -> None:
+        """."""
+        super().component_vectors_histogram(x, y)
+
+    def __str__(self) -> str:
+        """__str__."""
+        super().__str__()
+
+
+class DRVStatisticalResearch(BaseStatisticalResearch, MixinEmpiricalFunction, MixinHist):
+    """."""
+    def __init__(self) -> None:
+        pass
+
+    def M(self, x: list) -> float:
+        """Сheckmate waiting M(x)."""
+        return super().M(x)
+
+    def D(self, x: list) -> float:
+        """Dispersion D(x)."""
+        return super().D(x)
+
+    def M_x_y(self, x: list, y: list) -> float:
+        """Сheckmate waiting M(xy)"""
+        return super().M_x_y(x, y)
+
+    def corrcoef(self, x: list, y: list) -> None:
+        """Calculate and print corr coef."""
+        return super().corrcoef(x, y)
+
+    def theoretical_M(self, a: int, b: int, by='x') -> float:
+        """Сheckmate waiting theoretical_M(x)."""
+        l_f = lambda x, y: func(x, y) * (eval(by))
+        l_a = lambda x: a
+        l_b = lambda x: b
+        return integrate.dblquad(l_f, a, b, l_a, l_b)[0]
+
+    def theoretical_D(self, a: int, b: int, by='x') -> float:
+        """Dispersion theoretical_D(x)."""
+        l_f = lambda x, y: func(x, y) * (eval(by)) ** 2
+        l_a = lambda x: a
+        l_b = lambda x: b
+        return integrate.dblquad(l_f, a, b, l_a, l_b)[0] - self.theoretical_M(a, b, by) ** 2
+
+    def theoretical_M_x_y(self, a: int, b: int) -> float:
+        """Сheckmate waiting theoretical_M_x_y(xy)"""
+        return self.theoretical_M(a, b, by='x * y')
+
+    def theoretical_corrcoef(self, a:int, b: int) -> float:
+        """Calculate and print corr coef."""
+        return (self.theoretical_M_x_y(a, b) - self.theoretical_M(a, b, by='x') * self.theoretical_M(a, b, by='y')) / math.sqrt(self.theoretical_D(a, b, by='x') * self.theoretical_D(a, b, by='y'))
+
+    def function_interval(self, func: object, n: int, list_values: list) -> tuple:
+        """."""
+        return super().function_interval(func, n, list_values)
+
+    def component_vectors_histogram(self, x: list, y: list) -> None:
+        """."""
+        super().component_vectors_histogram(x, y)
 
     def __str__(self) -> str:
         """__str__."""
@@ -101,10 +248,13 @@ class BaseRandomVariable(ABC):
     def neumanns_method(self) -> list:
         """Neumann's method."""
         pairs = []
-        for _ in range(self.n):
-            pair = MultiplicativeCongruentMethod(2, 16807, 2 ** 31 - 1).start()
-            new_max_value = MultiplicativeCongruentMethod(1, 16807, 2 ** 31 - 1).start()[0] * self.max_value
-            pairs.append(pair if func(*pair) >= new_max_value else self.neumanns_method())
+        i = 0
+        while i < self.n:
+            pair = [random(), random()]
+            new_max_value = random() * self.max_value
+            if func(*pair) >= new_max_value:
+                pairs.append(pair)
+                i += 1
         return pairs
 
     @abstractmethod
@@ -118,11 +268,6 @@ class BaseRandomVariable(ABC):
         pass
 
     @abstractmethod
-    def run_testing_hypotheses(self) -> None:
-        """run testing hypotheses."""
-        pass
-
-    @abstractmethod
     def __str__(self) -> str:
         """__str__."""
         return self.__class__.__name__
@@ -130,14 +275,13 @@ class BaseRandomVariable(ABC):
 
 class ContinuousRandomVariable(BaseRandomVariable):
     """."""
-    def __init__(self,n: int, func: object, statistical_research: object, hypotheses: object) -> None:
+    def __init__(self,n: int, func: object, statistical_research: object) -> None:
         self.statistical_research: object = statistical_research
-        self.hypotheses: object = hypotheses
         self.func: object = func
         self.a: int = 0
         self.b: int = 1
         self.n: int = n
-        self.max_value = 16 / 7
+        self.max_value = 1
         self.pairs: list = []
 
     @property
@@ -150,16 +294,6 @@ class ContinuousRandomVariable(BaseRandomVariable):
         """Set statistical research."""
         self._statistical_research = CheckingInheritance.verify_for_object(statistical_research, BaseStatisticalResearch)
 
-    @property
-    def hypotheses(self) -> object:
-        """Get hypotheses."""
-        return self._hypotheses
-    
-    @hypotheses.setter
-    def hypotheses(self, hypotheses: object) -> None:
-        """Set hypotheses."""
-        self._hypotheses = CheckingInheritance.verify_for_object(hypotheses, BaseHypotheses)
-
     def neumanns_method(self) -> list:
         """Neumann's method."""
         self.pairs = super().neumanns_method()
@@ -170,11 +304,28 @@ class ContinuousRandomVariable(BaseRandomVariable):
 
     def run_statistical_research(self) -> None:
         """run statistical research."""
-        self.statistical_research.component_vectors_histogram(self.pairs, self.n)
+        x, y = list(zip(*self.pairs))
 
-    def run_testing_hypotheses(self) -> None:
-        """run testing hypotheses."""
-        pass
+        self.statistical_research.component_vectors_histogram(x, y)
+
+        print(f"M(x) = {self.statistical_research.M(x)}")
+        print(f"Interval M(x) = {self.statistical_research.function_interval(self.statistical_research.M, 1000, x)}")
+        print(f"Theoretical M(x) = {self.statistical_research.theoretical_M(self.a, self.b, by='x')}\n")
+    
+        print(f"D(x) = {self.statistical_research.D(x)}")
+        print(f"Interval D(x) = {self.statistical_research.function_interval(self.statistical_research.D, 1000, x)}")
+        print(f"Theoretical D(x) = {self.statistical_research.theoretical_D(self.a, self.b, by='x')}\n")
+
+        print(f"M(y) = {self.statistical_research.M(y)}")
+        print(f"Interval M(y) = {self.statistical_research.function_interval(self.statistical_research.M, 1000, y)}")
+        print(f"Theoretical M(y) = {self.statistical_research.theoretical_M(self.a, self.b, by='y')}\n")
+
+        print(f"D(y) = {self.statistical_research.D(y)}")
+        print(f"Interval D(y) = {self.statistical_research.function_interval(self.statistical_research.D, 1000, x)}")
+        print(f"Theoretical D(y) = {self.statistical_research.theoretical_D(self.a, self.b, by='y')}\n")
+
+        print(f"Corrcoef = {self.statistical_research.corrcoef(x, y)}")
+        print(f"Theoretical Corrcoef = {self.statistical_research.theoretical_corrcoef(self.a, self.b)}\n")
 
     def __str__(self) -> str:
         """__str__."""
@@ -183,14 +334,13 @@ class ContinuousRandomVariable(BaseRandomVariable):
 
 class DiscreteRandomVariable(BaseRandomVariable):
     """."""
-    def __init__(self,n: int, func: object, statistical_research: object, hypotheses: object) -> None:
+    def __init__(self,n: int, func: object, statistical_research: object) -> None:
         self.statistical_research: object = statistical_research
-        self.hypotheses: object = hypotheses
         self.func: object = func
         self.a: int = 0
         self.b: int = 1
         self.n: int = n
-        self.max_value = 16 / 7
+        self.max_value = 1
         self.pairs: list = []
 
     @property
@@ -203,16 +353,6 @@ class DiscreteRandomVariable(BaseRandomVariable):
         """Set statistical research."""
         self._statistical_research = CheckingInheritance.verify_for_object(statistical_research, BaseStatisticalResearch)
 
-    @property
-    def hypotheses(self) -> object:
-        """Get hypotheses."""
-        return self._hypotheses
-    
-    @hypotheses.setter
-    def hypotheses(self, hypotheses: object) -> None:
-        """Set hypotheses."""
-        self._hypotheses = CheckingInheritance.verify_for_object(hypotheses, BaseHypotheses)
-
     def neumanns_method(self) -> list:
         """Neumann's method."""
         self.pairs = super().neumanns_method()
@@ -223,80 +363,28 @@ class DiscreteRandomVariable(BaseRandomVariable):
 
     def run_statistical_research(self) -> None:
         """run statistical research."""
-        self.statistical_research.component_vectors_histogram(self.pairs, self.n)
+        x, y = list(zip(*self.pairs))
 
-    def run_testing_hypotheses(self) -> None:
-        """run testing hypotheses."""
-        pass
+        self.statistical_research.empirical_function(x, y)
 
-    def __str__(self) -> str:
-        """__str__."""
-        super().__str__()
+        print(f"M(x) = {self.statistical_research.M(x)}")
+        print(f"Interval M(x) = {self.statistical_research.function_interval(self.statistical_research.M, 1000, x)}")
+        print(f"Theoretical M(x) = {self.statistical_research.theoretical_M(self.a, self.b, by='x')}\n")
+    
+        print(f"D(x) = {self.statistical_research.D(x)}")
+        print(f"Interval D(x) = {self.statistical_research.function_interval(self.statistical_research.D, 1000, x)}")
+        print(f"Theoretical D(x) = {self.statistical_research.theoretical_D(self.a, self.b, by='x')}\n")
 
+        print(f"M(y) = {self.statistical_research.M(y)}")
+        print(f"Interval M(y) = {self.statistical_research.function_interval(self.statistical_research.M, 1000, y)}")
+        print(f"Theoretical M(y) = {self.statistical_research.theoretical_M(self.a, self.b, by='y')}\n")
 
-class BaseStatisticalResearch(ABC):
-    """."""
-    @abstractmethod
-    def component_vectors_histogram(self, pairs: list, n: int) -> None:
-        """."""
-        pass
+        print(f"D(y) = {self.statistical_research.D(y)}")
+        print(f"Interval D(y) = {self.statistical_research.function_interval(self.statistical_research.D, 1000, x)}")
+        print(f"Theoretical D(y) = {self.statistical_research.theoretical_D(self.a, self.b, by='y')}\n")
 
-    @abstractmethod
-    def __str__(self) -> str:
-        """__str__."""
-        return self.__class__.__name__
-
-
-class CRVStatisticalResearch(BaseStatisticalResearch):
-    """."""
-    def __init__(self) -> None:
-        pass
-
-    def component_vectors_histogram(self, pairs: list, n: int) -> None:
-        """."""
-        super().component_vectors_histogram(pairs, n)
-
-    def __str__(self) -> str:
-        """__str__."""
-        super().__str__()
-
-
-class DRVStatisticalResearch(BaseStatisticalResearch):
-    """."""
-    def __init__(self) -> None:
-        pass
-
-    def component_vectors_histogram(self, pairs: list, n: int) -> None:
-        """."""
-        super().component_vectors_histogram(pairs, n)
-
-    def __str__(self) -> str:
-        """__str__."""
-        super().__str__()
-
-
-class BaseHypotheses(ABC):
-    """."""
-    @abstractmethod
-    def __str__(self) -> str:
-        """__str__."""
-        return self.__class__.__name__
-
-
-class CRVHypotheses(BaseHypotheses):
-    """."""
-    def __init__(self) -> None:
-        pass
-
-    def __str__(self) -> str:
-        """__str__."""
-        super().__str__()
-
-
-class DRVHypotheses(BaseHypotheses):
-    """."""
-    def __init__(self) -> None:
-        pass
+        print(f"Corrcoef = {self.statistical_research.corrcoef(x, y)}")
+        print(f"Theoretical Corrcoef = {self.statistical_research.theoretical_corrcoef(self.a, self.b)}\n")
 
     def __str__(self) -> str:
         """__str__."""
@@ -328,16 +416,6 @@ class LAB:
         for rv in self.random_variables:
             rv.run()
             rv.run_statistical_research()
-            rv.run_testing_hypotheses()
-
-
-class Test:
-    def __init__(self):
-        pass
-
-    def __str__(self) -> str:
-        """__str__."""
-        return self.__class__.__name__
 
 
 def main() -> None:
@@ -345,20 +423,16 @@ def main() -> None:
     crv_statistical_research = CRVStatisticalResearch()
     drv_statistical_research = DRVStatisticalResearch()
 
-    crv_hypotheses = CRVHypotheses()
-    drv_hypotheses = DRVHypotheses()
-
     continuous_random_variable = ContinuousRandomVariable(
-        10000,
+        1000,
         func,
-        crv_statistical_research,
-        crv_hypotheses,
+        crv_statistical_research
     )
+
     discrete_random_variable = DiscreteRandomVariable(
-        10000,
+        1000,
         func,
-        drv_statistical_research,
-        drv_hypotheses
+        drv_statistical_research
     )
 
     lab = LAB()
